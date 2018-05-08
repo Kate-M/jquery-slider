bbbef05f58d9b3e6850b778343c33b0e0a9f2e7a
Kate-M <ekaterina.minich@gmail.com>
2018-05-08 20:20:04 +0300

    feature: added some code for main controle buttons

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,15 @@
 
+(function () {
+    // $('.content-controls').css('overflow', 'hidden');
+
+    $('.main-controls').find('.btn-main').on('click', function () {
+        let category = $(this).data('cotegory');
+        console.log(category);
+        let marginConteiner = parseInt(0, $('.content-controls').css('marginTop'));
+        console.log(marginConteiner);
+        $('.content-controls').animate({
+            'margin-top': marginConteiner - 420
+        });
+    });
+}());
+
